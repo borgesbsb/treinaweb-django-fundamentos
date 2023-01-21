974c3f0f63f5736df6ab0b723892aaf6c4c9f69f
Benjamin  da Silva Borges <borges.bnjamin@gmail.com>
2023-01-21 12:47:00 -0400

    add: insert data in intitites

---
+++ clientes/services/endereco_service.py
@@ -0,0 +1,10 @@
+from ..models import Endereco
+
+def cadastrar_endereco(endereco):
+    return Endereco.objects.create(rua=endereco.rua, 
+                            numero=endereco.numero,
+                            complemento=endereco.complemento,
+                            bairro=endereco.bairro,
+                            cidade=endereco.cidade,
+                            pais=endereco.pais
+                            )
